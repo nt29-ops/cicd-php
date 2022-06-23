bf1d42a1b6c6bf2fd101f605f13f3eff0349b9a1
nt29-ops <84270122+nt29-ops@users.noreply.github.com>
2022-06-23 09:42:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 <h1>Dev Team</h1>
 
 <?php echo "The Best PHP5"; ?>
-
+<?php echo "The Best PHP5"; ?>
+<?php echo "The Best PHP5"; ?>
+<?php echo "The Best PHP5"; ?>
+<?php echo "The Best PHP5"; ?>
+  
 </body>
 </html> 
